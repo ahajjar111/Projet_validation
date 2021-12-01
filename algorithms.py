61ac886c029925592e7812af46f54a8b81ca6523
Ayman Hajjar <aymanhajjar9@gmail.com>
2021-12-01 17:28:40 +0100

    Modification algorithms

--- algorithms.py
+++ algorithms.py
@@ -1,14 +1,17 @@
 def is_safe_bfs(g):
     known = [] #set()
     frontier = [] #depue()
-    acct_start = True
-    while frontier or acct_start:
-        if acct_start:
+    at_start = True
+    while frontier or at_start:
+        if at_start:
             neighbours = g.initial()
-        else g.next(frontier.popleft())
-    for n in neighbours:
-        if g.is_accepting(n):
-            return False
-        if n not in known:
-            known.append(n)
-            frontier.append(n)
+            at_start = False
+        else: g.next(frontier.pop(0))
+
+        for n in neighbours:
+            if g.is_accepting(n):
+                return True,n
+            if n not in known:
+                known.append(n)
+                frontier.append(n)
+    return False,n
