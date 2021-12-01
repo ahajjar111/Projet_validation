b0a0a70a2f128596fa4234b17444657eabfdcb5a
Ayman Hajjar <aymanhajjar9@gmail.com>
2021-12-01 17:40:53 +0100

    Modif script kernel

--- Kernel.py
+++ Kernel.py
@@ -16,3 +16,15 @@ class identifyProxy:
 
     def __getattribute__(self,attr):
         return getattr(self.operand, attr)
+
+class ParentStoreProxy(identifyProxy):
+    def __init__(self, operand):
+        super().__init__(operand)
+        self.parent = {}
+    
+    def next(self, conf):
+        ns = self.operand.next(conf)
+        for n in ns:
+            if n not in self.parents:
+                self.parents[n]=conf,None
+        return ns
