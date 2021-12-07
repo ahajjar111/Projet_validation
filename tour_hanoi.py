163cebf681f82468a3a6e8570cdfb71278e5b975
Ayman Hajjar <aymanhajjar9@gmail.com>
2021-12-08 00:03:03 +0100

    Class stack

---
+++ tour_hanoi.py
@@ -0,0 +1,12 @@
+import sys
+
+
+# A structure to represent a stack
+class Stack:
+    # Constructor to set the data of
+    # the newly created tree node
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.top = -1
+        self.array = [0] * capacity
+
