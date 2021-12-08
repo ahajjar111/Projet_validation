b344555d1e1417b9052ed57ef3d66d0890bc17e1
Ayman Hajjar <aymanhajjar9@gmail.com>
2021-12-08 14:58:09 +0100

    ajout class semanticTransitionRelations

--- Kernel.py
+++ Kernel.py
@@ -28,3 +28,28 @@ class ParentStoreProxy(identifyProxy):
             if n not in self.parents:
                 self.parents[n]=conf,None
         return ns
+
+
+class semanticTransitionRelations:
+    def __init__(self):
+        pass
+
+    def actions(self, conf):
+        pass
+
+    def execute(self, conf, actions):
+        pass
+
+class BehSoupSemantics(semanticTransitionRelations):
+    def initial(self):
+        return [soup.initial]
+
+    def actions(self, conf):
+        return list(map(lambda beh : beh.action,
+                         filter(lambda beh: beh.guard(conf),
+                         self.soup.behaviours)))
+
+    def execute(self, c, a):
+        target = copy.deepcopy(c)
+        r = a(target)
+        return target
