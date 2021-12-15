ec3e2b92805f2e9df0f9ffbe5988ac6eecf7dd92
Ayman Hajjar <aymanhajjar9@gmail.com>
2021-12-15 17:03:17 +0100

    Add counter function and main

--- Counter.py
+++ Counter.py
@@ -1,8 +1,32 @@
+from Kernel import STR2TR, isAcceptingProxy
+from SoupLanguage import BehaviourSoup, BehSoupSemantics
+from algorithms import predicate_model_checker
+
+
 class CounterConfig:
     def __init__(self):
         self.pc = 0
-
-    def hash(self):
+    def __hash__(self):
         return hash(self.pc)
+    def __eq__(self, other):
+        return self.pc==other.pc
 
-    
+
+def counter(max):
+    soup = BehaviourSoup(CounterConfig())
+    def inc(c):
+        c.pc = c.pc + 1
+    soup.add('inc', lambda c: c.pc < max , inc)
+    def reset(c):
+        c.pc = 0
+    soup.add('reset', lambda c: c.pc>=max , reset)
+
+    return soup
+
+if __name__ == '__main__':
+    semantics = BehSoupSemantics(counter(3))
+    # predicate_model_checker(semantics, lambda c: c.pc==2)
+    # predicate_model_checker(semantics, lambda c: c.pc > 50)
+    tr = STR2TR(semantics)
+    tr = isAcceptingProxy(tr,lambda c: c.pc==2)
+    print(tr.initial())
