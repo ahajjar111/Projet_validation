fd0b526cfec015cd19db7910ef5aa0790fd8409a
Ayman Hajjar <aymanhajjar9@gmail.com>
2022-01-10 14:15:13 +0100

    Ajout fonction bfs

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,24 @@
 from Kernel import STR2TR, isAcceptingProxy, ParentStoreProxy
+from typing import Deque
+
+def bfs(graph):
+    known = set()  # Known
+    frontier = Deque()  # Frontier #list_des_noeuds
+    at_start = True  # init
+
+    while frontier or at_start:
+        if at_start:
+            neighbours = graph.initial()
+            at_start = False
+        else:
+            neighbours = graph.next(frontier.popleft())
+        for n in neighbours:
+            
+            if n not in known:
+                known.add(n)
+                frontier.append(n)
+
+    return known
 
 def find_accepting_bfs(g):
 
