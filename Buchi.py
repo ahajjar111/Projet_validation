2d821c4cdfb9f1d7b57afb86249de0530bab2e0f
HusSaad <husseinsaadgs@gmail.com>
2022-01-10 17:27:31 +0100

    ajouter buchi algorithme

---
+++ Buchi.py
@@ -0,0 +1,22 @@
+class BuchiSemantics(iSTR):
+    initial(self):
+    set self.initial
+    actions(self, i, c):
+    actions= self.delta[c]
+        for a in actions:
+            if a[0](i):
+                active.apprend(a)
+        return actives
+
+__init(buchi)__
+    ini, d, pred
+    self.initial = ini
+    self.delta=dbmself.pred=pred
+
+
+
+execute(self,i,c,a)
+
+return[a]
+
+
