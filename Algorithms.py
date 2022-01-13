61c82c9695c195457f51c21f67438173d9957aaa
Ayman Hajjar <aymanhajjar9@gmail.com>
2022-01-13 22:01:11 +0100

    Qlq modif de commentaires

--- Algorithms.py
+++ Algorithms.py
@@ -1,17 +1,17 @@
 from Kernel import STR2TR, isAcceptingProxy, ParentStoreProxy
 from typing import Deque
 
-def bfs(graph):
-    known = set()  # Known
-    frontier = Deque()  # Frontier #list_des_noeuds
+def bfs(g):
+    known = set()
+    frontier = Deque() 
     at_start = True  # init
 
     while frontier or at_start:
         if at_start:
-            neighbours = graph.initial()
+            neighbours = g.initial()
             at_start = False
         else:
-            neighbours = graph.next(frontier.popleft())
+            neighbours = g.next(frontier.popleft())
         for n in neighbours:
             
             if n not in known:
@@ -23,14 +23,14 @@ def bfs(graph):
 def find_accepting_bfs(g):
 
     known = dict() 
-    frontier = [] #depue()
+    frontier = Deque()
     at_start = True
 
     while frontier or at_start:
         if at_start:
             neighbours = g.initial()
             at_start = False
-        else: g.next(frontier.pop(0))
+        else: neighbours = g.next(frontier.popleft())
 
         for n in neighbours:
             if n not in known:
@@ -38,7 +38,52 @@ def find_accepting_bfs(g):
                     return True,n
                 known[n] = n
                 frontier.append(n)
-    return False,n
+    return False,None
+
+def is_bfs_reachable(g, start, end):
+    known = set()
+    frontier = Deque()
+    neighbours = [start]
+
+    for i in neighbours:
+        if i not in known:
+            known.add(i)
+            frontier.append(i)
+
+    while frontier:
+        neighbours = g.next(frontier.popleft())
+
+        for i in neighbours:
+            if i == end:
+                return True
+            if i not in known:
+                known.add(i)
+                frontier.append(i)
+
+    return False
+
+
+def is_bfs_safe(graph):
+    known = set()
+    frontier = Deque()
+    at_start = True
+    
+    while frontier or at_start:
+        if at_start:
+            neighbours = [graph.initial()]
+            at_start = False
+        else:
+            neighbours = graph.next(frontier.popleft())
+        for n in neighbours:
+            
+            if n not in known:
+                if graph.is_accepting(n):
+                    return False
+                known[n] = n
+                frontier.append(n)
+
+    return True
+
 
 def predicate_model_checker(semantics, predicate):
     tr = STR2TR(semantics)
