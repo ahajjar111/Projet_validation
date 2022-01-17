1873012858823f026f92b71b0a62f172191325de
Ayman Hajjar <aymanhajjar9@gmail.com>
2022-01-17 09:40:55 +0100

    Organisation

--- Algorithms.py
+++ Algorithms.py
@@ -66,3 +66,43 @@ def get_trace(parents, result, initial):
         trace.append(current_Node)
 
     print("Trace : ", trace)
+
+def is_bfs_reachable(graph, start, end):
+    known = set()  # Known
+    frontier = Deque()  # Frontier #list_des_noeuds
+    neighbours = [start]
+    for i in neighbours:
+        if i not in known:
+            known.add(i)
+            frontier.append(i)
+    while frontier:
+        neighbours = graph.next(frontier.popleft())
+        for i in neighbours:
+            if i == end:
+                return True
+            if i not in known:
+                known.add(i)
+                frontier.append(i)
+    return False
+
+
+def is_bfs_safe(graph):
+    known = set()  # Known
+    frontier = Deque()  # Frontier #list_des_noeuds
+    at_start = True  # init
+
+    while frontier or at_start:
+        if at_start:
+            neighbours = [graph.initial()]
+            at_start = False
+        else:
+            neighbours = graph.next(frontier.popleft())
+        for n in neighbours:
+            
+            if n not in known:
+                if graph.is_accepting(n):
+                    return False
+                known[n] = n
+                frontier.append(n)
+
+    return True
